bfe606e8b1a4f11c9c0a7dd74920eda51474332d
Salma158 <salmasherif856@gmail.com>
2024-07-02 21:03:30 +0300

    rising temperature problem using subquery

--- Leetcode_Study_Plan/Basic Joins/Rising Temperature.sql
+++ Leetcode_Study_Plan/Basic Joins/Rising Temperature.sql
@@ -1,4 +1,15 @@
+-- using self join
 SELECT W2.id
 FROM Weather W2
 JOIN Weather W1 ON DATEDIFF(W2.recordDate, W1.recordDate) = 1
 WHERE W2.temperature > W1.temperature;
+
+
+-- using subquery
+SELECT W1.id
+FROM Weather W1
+WHERE W1.temperature > (
+    SELECT W2.temperature
+    FROM Weather W2
+    WHERE W2.recordDate = DATE_SUB(W1.recordDate, INTERVAL 1 DAY)
+);
